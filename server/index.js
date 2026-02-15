4a6ac9f73c8e0bde2543a0053a91832f3222c34c
smansayeed <786saadman@gmail.com>
2026-02-15 18:06:47 +0600

    completed

--- server/index.js
+++ server/index.js
@@ -12,7 +12,9 @@ app.use(cors());
 
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+app.use("/api",router);
+
+const PORT = process.env.PORT || 5001;
 
 const connectDB = async () => {
   try {
